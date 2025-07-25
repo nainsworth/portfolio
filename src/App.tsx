e033ce5683fbb2b6ec3c272f86e98b2ca63aa5e6
nainsworth <nja8161995@gmail.com>
2025-07-24 19:49:47 -0500

    Modified Styling and created added coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,21 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p className="text-5xl text-green-800">Portfolio: Tailwind Test</p>
+      <div className="flex flex-col gap-5">
+        <h1 className="text-7xl">Portfolio</h1>
+        <h2 className="text-4xl">Coming Soon</h2>
+        <p className="text-xl mt-8">
+          Meanwhile, please checkout my{" "}
+          <a
+            className="text-blue-500 hover:text-white hover:no-underline underline"
+            href="https://github.com/nainsworth"
+            target="_blank"
+          >
+            github
+          </a>
+          !
+        </p>
+      </div>
       <Analytics />
     </>
   );
