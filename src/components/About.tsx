28e0ba6a7c5a454badcf867ee307984d43c31256
nainsworth <nja8161995@gmail.com>
2025-07-25 21:56:10 -0500

    Fixed layering

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,22 +1,22 @@
 const About = () => {
   return (
     <section id="about">
-        {/* Background Effects */}
+      {/* Background Effects */}
       {/* <div className="inset-0 bg-gray-900 z-0 absolute"> */}
-      <div>
+      <div className="z-0">
         <div className="w-64 h-64 bg-blue-600/10 rounded-full blur-3xl absolute top-20 left-10"></div>
         <div className="w-96 h-96 bg-purple-600/10 rounded-full blur-3xl absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"></div>
         <div className="w-80 h-80 bg-indigo-600/10 rounded-full blur-3xl absolute bottom-10 right-10"></div>
         {/* <div className="inset-0 bg-grid-pattern opacity-[0.03] z-0 absolute"></div> */}
       </div>
 
-      <div className="flex flex-col gap-5 place-items-center min-h-screen self-center justify-center pb-50">
+      <div className="flex flex-col gap-5 place-items-center min-h-screen self-center justify-center z-50 absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
         <h1 className="text-7xl">Portfolio</h1>
         <h2 className="text-4xl">Coming Soon</h2>
         <p className="text-xl mt-8">
           Meanwhile, please checkout my{" "}
           <a
-            className="text-blue-500 hover:text-white hover:no-underline underline"
+            className="text-blue-500 hover:text-white hover:no-underline underline z-50"
             href="https://github.com/nainsworth"
             target="_blank"
           >
