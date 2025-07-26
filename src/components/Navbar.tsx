e779c58b21e73fdf1375c7ba856d3e24e2cade6e
nainsworth <nja8161995@gmail.com>
2025-07-25 21:07:27 -0500

    Styled nav link hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,31 +6,46 @@ const Navbar = () => {
 const [isOpen, setOpen] = useState(false);
 
   return (
-    <div className="w-full flex justify-between items-center py-6 px-12 xl:w-7xl xl:px-0">
+    <div className="w-full flex justify-between items-center mx-auto py-6 px-12 xl:w-7xl xl:px-0">
       <div>
         <p className="text-xl">Portfolio Logo</p>
       </div>
 
       <div>
         <div className="sm:hidden">
-          <Hamburger toggled={isOpen} toggle={setOpen} direction="right" size={26} />
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            direction="right"
+            size={26}
+          />
         </div>
 
         <ul className="hidden sm:flex gap-8">
           <li>
-            <a href="#">About</a>
+            <a className="navLink" href="#">
+              About
+            </a>
           </li>
           <li>
-            <a href="#">Projects</a>
+            <a className="navLink" href="#">
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#">Experience</a>
+            <a className="navLink" href="#">
+              Experience
+            </a>
           </li>
           <li>
-            <a href="#">Skills</a>
+            <a className="navLink" href="#">
+              Skills
+            </a>
           </li>
           <li>
-            <a href="#">Contact</a>
+            <a className="navLink" href="#">
+              Contact
+            </a>
           </li>
         </ul>
       </div>
