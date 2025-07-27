78b1ed632d8978505255ae2d6ead44c1334da0c4
nainsworth <nja8161995@gmail.com>
2025-07-26 21:27:10 -0500

    Fixed styling Issue

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,18 +4,18 @@ const About = () => {
   return (
     <section
       id="about"
-      className="flex items-center pt-24 px-4 overflow-hidden min-h-screen"
+      className="flex items-center pt-24 px-4 overflow-hidden min-h-screen relative"
     >
       {/* Background Effects */}
       {/* <div className="inset-0 bg-gray-900 z-0 absolute"> */}
-      <div className="z-0">
+      <div className="z-0 absolute inset-0">
         <div className="w-64 h-64 bg-blue-600/10 rounded-full blur-3xl absolute top-20 left-10"></div>
-        <div className="w-96 h-96 bg-purple-600/10 rounded-full blur-3xl absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"></div>
+        <div className="w-96 h-96 bg-purple-600/5 rounded-full blur-3xl absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"></div>
         <div className="w-80 h-80 bg-indigo-600/10 rounded-full blur-3xl absolute bottom-10 right-10"></div>
         {/* <div className="inset-0 bg-grid-pattern opacity-[0.03] z-0 absolute"></div> */}
       </div>
 
-      <div className="container mx-auto max-w-5xl flex flex-col md:flex-row gap-12 z-10">
+      <div className="container mx-auto max-w-5xl flex flex-col items-center md:flex-row gap-12 z-10">
         <div className="md:w-1/2 relative">
           <div className="inset-0 bg-gradient-to-r from-blue-500/20 to-purple-500/20 blur-2xl rounded-full absolute"></div>
           <img
